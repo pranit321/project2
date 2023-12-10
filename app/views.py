374f04ab275c1269b242d6a3e33b84edf3456ded
pranit <pranitsahu2000@gmail.com>
2023-12-10 16:34:46 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def lalala(request):
+    return render(request,'one.html')
+def thala(request):
+    return render(request,'two.html')
+def kalala(request):
+    return render(request,'three.html')
+def chini(request):
+    return render(request,'four.html')
